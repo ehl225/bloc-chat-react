92458f6e9e96ba375d2b08da83264ecf1dee449e
Erica <ehlam225@gmail.com>
2019-07-18 19:52:31 -0400

    Created a list of chat rooms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
-function App() {
-  return (
+//The core Firebase JS SDK is always required and must be 
+//listed first 
+//<script src = 
+//"https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js" >
+//</script>
+//TODO: Add SKDs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#config-web-app
+
+//Your web app's Firebase configuration
+var firebaseConfig = {
+//	src: 
+//"https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js",
+	apiKey: "AIzaSyBxLjymw75SqIlvQUz9XFs5SwdviM1P7nk", 
+	authDomain: "bloc-chat-react-ehl.firebaseapp.com", 
+	databaseURL: "https://bloc-chat-react-ehl.firebaseio.com",
+	projectId: "bloc-chat-react-ehl", 
+	storageBucket: "",
+	messagingSenderId: "1074466121746", 
+	appId: "1:1074466121746:web:7b3fe1edd994fcb5"
+};
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+class App extends Component {
+render () {
+ return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<h1>Bloc Chat</h1>
       </header>
-    </div>
+	<main>
+		<RoomList firebase= {firebase} />
+    	</main>
+	</div>
+
   );
+}
 }
 
 export default App;
---
+++ src/components/RoomList.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
+
+class RoomList extends Component {
+constructor(props) {
+	super(props);
+		this.state= {
+		rooms: [] 
+};
+	this.roomsRef = this.props.firebase.database().ref('rooms');
+
+}
+componentDidMount () {
+	this.roomsRef.on('child_added', snapshot => {
+	const room = snapshot.val();
+	room.key = snapshot.key;
+	this.setState( {rooms: this.state.rooms.concat(room)});
+	});
+}
+
+render () {
+return (
+<div className="roomList">
+{this.state.rooms.map(room => 
+	<div className="room" key={room.key}> 
+	{room.name}
+	</div>
+	)}
+</div>
+);
+}
+}
+export default RoomList;
+
